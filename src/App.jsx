dbdf09222d49fd75fcb725427c4def3dc5ce13bb
jayantWishlist <jayant@enjoywishlist.com>
2025-01-08 15:35:47 +0530

    iphone website

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Hero from './components/hero'
+import Highlights from './components/Highlights'
+import Navbar from './components/navbar'
+
+const App=()=> {
+
+  return (
+   <main className='bg-black'>
+    <Navbar/>
+    <Hero/>
+    <Highlights/>
+   </main>
+  )
+}
+
+export default App
